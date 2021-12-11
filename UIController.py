13b32bb967a9c98a8d7b5b8b34a82cad728c4e81
Nikola <nsretkovic@elfak.rs>
2021-12-12 00:52:22 +0100

    Colorize walls and players

--- UIController.py
+++ UIController.py
@@ -1,4 +1,7 @@
 import os
+#from termcolor import colored
+from ColorizeText import colored
+
 
 clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
@@ -13,16 +16,29 @@ def printGameMatrix(rows, cols, playerFields, hWalls, vWalls):
         if(j < cols):
           h_fieldNumbers+= str(j+1) if (j+1 < 10) else str(chr(65+j-9))
           h_fieldNumbers+= "  "
-        h_Wals += hWalls[i][j] +" " 
+        #h_Wals += hWalls[i][j] +" "
+        h_Wals += hWalls[i][j] if hWalls[i][j] == "--" else colored(hWalls[i][j], 'green')
+        h_Wals += " " 
       if(i > 0):
-        fields_and_V_Wals += playerFields[i][j] + vWalls[i][j]
-        h_Wals +=hWalls[i][j] + " "
+        #fields_and_V_Wals += playerFields[i][j] + vWalls[i][j]
+        playerColored = ""
+        if "X" in playerFields[i][j]:
+          playerColored = colored(playerFields[i][j], 'red', attrs=['bold'])
+        elif "O" in playerFields[i][j]:
+          playerColored = colored(playerFields[i][j], 'yellow', attrs=['bold'])
+        else:
+          playerColored = playerFields[i][j]
+        fields_and_V_Wals += playerColored
+        fields_and_V_Wals += vWalls[i][j] if vWalls[i][j] == "|" else colored(vWalls[i][j], 'blue')
+        #h_Wals +=hWalls[i][j] + " "
+        h_Wals += hWalls[i][j] if hWalls[i][j] == "--" else colored(hWalls[i][j], 'green')
+        h_Wals += " " 
     if(j == cols):
       fields_and_V_Wals += " "
       fields_and_V_Wals += str(i) if (i < 10) else str(chr(65+i-10))
     if(i==0):
       print(h_fieldNumbers)
-      print(h_Wals)
+      print(colored(h_Wals))
     else:
       print(fields_and_V_Wals)
       print(h_Wals)
