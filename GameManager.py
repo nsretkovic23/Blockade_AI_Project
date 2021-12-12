b8aa079389a5ad3a760daa52b841f00fc818b0bc
Nikola <nsretkovic@elfak.rs>
2021-12-12 17:29:09 +0100

    Implement complete logic for player movement

--- GameManager.py
+++ GameManager.py
@@ -1,5 +1,6 @@
-from os import error
+from External_Packages.ColorizeText import colored
 from NewGameGenerator import CreateNewGame
+from PawnMovementController import setNewPawnPosition, getPawnsCurrentPosition, markStartingPosition, canPawnMoveThere
 
 class GameState:
      def __init__(self):
@@ -55,47 +56,77 @@ def setPawnsOnStartingPositions(playingFields, xPos, oPos):
         if(i < 2):
             playingFields[xPos[i][0]][xPos[i][1]] = f"X{i+1}"
         else:
-            playingFields[oPos[i%2][0]][oPos[i%2][1]] = f"O{i+1}"
+            playingFields[oPos[i%2][0]][oPos[i%2][1]] = f"O{i%2+1}"
 
-# TODO: Move: checkIfCanMove, checkIfCanPlaceWall
-# Format: [["X1"], [5,7], ["h", 3,4]]
-def playTurn(state:GameState, turn):
-    currentPosition = []
-    if(turn[0][0] == "X1"):
-        currentPosition = state.x1Pos
-    elif(turn[0][0] == "X2"):
-        currentPosition = state.x2Pos
-    elif(turn[0][0] == "O1"):
-        currentPosition = state.o1Pos
-    elif(turn[0][0] == "O2"):
-        currentPosition = state.o2Pos
-    else:
-        error("Wrong pawn")
-    state.playingFields[currentPosition[0]][currentPosition[1]] = "  "
-
-    state.playingFields[turn[1][0]][turn[1][1]] = turn[0][0]
-
-    # TODO: Needs only one more check if there is a wall
-    if(turn[2][0] == "h" and turn[2][1] > 0 and turn[2][1] <= state.rows and turn[2][2] < state.cols-1 and turn[2][2] > 0):
-        state.hWalls[turn[2][1]][turn[2][2]] = "=="
-        state.hWalls[turn[2][1]][turn[2][2]+1] = "=="
-
-# TODO: Implement checkers to give an error before it goes to playTurn
-#       Rename this to playTurn and playTurn to Execute Turn
-def serializeTurn():
+def playTurn(state:GameState):
     move = []
     player = ""
+    pawnCurrentPosition = []
     wall = []
+    isMoveValid = False
+    isWallValid = False
+    isFirstInputDoneAlready = False
+    row = 0
+    column = 0
 
     while(player != "X1" and player != "X2" and player != "O1" and player != "O2"):
         player = input("Select player: ")
 
-    # TODO: Checkers if out of row/col
-    row = int(input("Select row: "))
-    column = int(input("Select column: "))
-    move = [row, column]
+    while(isMoveValid is False):
+        if(row < 1 or row > state.rows):
+            if isFirstInputDoneAlready is True:
+                print(colored("Wrong positions, try again!", 'red'))
+
+            asciiToNumber = ord(input(colored("Select row: ", 'cyan')).upper())
+            row = asciiToNumber - 48 if (asciiToNumber < 58) else asciiToNumber - 55
+            
+            isFirstInputDoneAlready = True
+        else:
+            move.append(row)
+
+        if(column < 1 or column > state.cols):
+           
+            asciiToNumber = ord(input(colored("Select column: ", 'cyan')).upper())
+            column = asciiToNumber - 48 if (asciiToNumber < 58) else asciiToNumber - 55
+        
+        else:
+            move.append(column)
+            # Assume that move is valid because cols and rows are in dimensions range:
+            isMoveValid = True
+
+
+        pawnCurrentPosition = getPawnsCurrentPosition(player, state)
+
+        if isMoveValid is True:
+            if canPawnMoveThere(pawnCurrentPosition, row, column) is True:
+                isMoveValid = True
+            else:
+                print(colored("You can't move there, try again!", 'red', attrs=['bold']))
+                isMoveValid = False
+                row = 0
+                column = 0
+
     # TODO: Check if has walls left
     wall.append(input("Select wall, Type v/h : ").lower())
     wall.append(int(input("Select wall's starting row: ")))
     wall.append(int(input("Select wall's starting column: ")))
     return [[player], move, wall]
+
+
+def exectuteTurn(state:GameState, turn):
+    currentPosition = getPawnsCurrentPosition(turn[0][0], state)
+
+    state.playingFields[currentPosition[0]][currentPosition[1]] = "  "
+
+    state.playingFields[turn[1][0]][turn[1][1]] = turn[0][0]
+
+    #Check if pawn moved from STARTING and set to XX / OO
+    markStartingPosition(turn[0][0], currentPosition, state)
+
+    setNewPawnPosition(turn[0][0], state, turn[1])
+
+    # TODO: Needs only one more check if there is a wall
+    if(turn[2][0] == "h"):
+        state.hWalls[turn[2][1]][turn[2][2]] = "=="
+        state.hWalls[turn[2][1]][turn[2][2]+1] = "=="
+
