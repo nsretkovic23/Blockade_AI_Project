b89582a97eae6a77af93f504ec0ae626f20b7836
Nikola <nsretkovic@elfak.rs>
2021-12-12 03:53:54 +0100

    Implement game beginning logic

--- Gameplay.py
+++ Gameplay.py
@@ -1,11 +1,69 @@
-import sys
-from UIController import clearConsole
-from GameManager import GameManager
+from UIController import clearConsole, printGameMatrix
+from GameManager import GameState, getBeginningState
+from External_Packages.ColorizeText import colored
+from Cheats import loadDefaultGameplay
 
-state = GameManager()
+whoPlaysFirst = ""
+numberOfWalls = 0
+rows = 0
+cols = 0
+xPositions = []
+oPositions = []
+positionInput = 0
 
-print("Welcome to the BLOCKADE GAME!")
-print("Who do you want to play first? Type me/cpu")
+# CHEAT - LOAD DEFAULT GAME INSTANTLY FOR TESTING
+defaultGame = loadDefaultGameplay()
+printGameMatrix(defaultGame)
 
-if(input("who plays") == "cpu"):
-    clearConsole()
+
+
+print(colored("\n-----------------------------------", 'red', attrs=['bold']))
+print(colored(" Welcome to the BLOCKADE GAME!", 'red', attrs=['bold']))
+print(colored("-----------------------------------\n", 'red', attrs=['bold']))
+
+while(whoPlaysFirst != 'me' and whoPlaysFirst !='cpu'):
+    whoPlaysFirst = input(colored("Who do you want to play first? Type me/cpu: ", 'cyan'))
+
+print(colored("\nLet's create the table!", 'yellow')) 
+print(" Tip: Recommended dimensions are 14x11\n Rule: Max dimensions are 28x22, Minimum dimensions are 10x8!\n")
+while(rows < 10 or rows > 28):
+    rows = int(input(colored("Enter the number of rows: ", 'cyan')))
+
+while(cols < 8 or cols > 22):
+    cols = int(input(colored("Enter the number of columns: ", 'cyan')))
+
+print(colored("\nGood choice! Let's choose the number of walls!", 'yellow'))
+print(" Tip: Recommended number is 9\n Rule: Maximum is 18, Minimum is 5!\n")
+
+while(numberOfWalls < 5 or numberOfWalls > 18):
+    numberOfWalls = int(input(colored("Enter number of walls: ", 'cyan')))
+
+
+print(colored("\nGreat! Now let's set starting positions!", 'yellow'))
+
+for i in range (0,4):
+    while(positionInput < 1 or positionInput > rows):
+        if(i < 2):
+            positionInput = int(input(colored(f"X{i%2 + 1} row: ", 'cyan')))
+            if(positionInput > 0 and positionInput <= rows):
+                xPositions.append([positionInput])
+        else:
+            positionInput = int(input(colored(f"O{i%2 + 1} row: ", 'cyan')))
+            if(positionInput > 0 and positionInput <= rows):
+                oPositions.append([positionInput])
+    positionInput = 0
+    while(positionInput < 1 or positionInput > cols):
+        if(i<2):
+            positionInput = int(input(colored(f"X{i%2 + 1} column: ", 'cyan')))
+            if(positionInput > 0 and positionInput <= cols):
+                xPositions[i].append(positionInput)
+        else:
+            positionInput = int(input(colored(f"O{i%2 + 1} column: ", 'cyan')))
+            if(positionInput > 0 and positionInput <= cols):
+                oPositions[i%2].append(positionInput)
+    positionInput = 0
+
+state = getBeginningState(whoPlaysFirst, numberOfWalls, rows, cols, xPositions, oPositions)
+
+printGameMatrix(state)
+print("success")
