b467ebb20e4f5bc9086d5df814230b8e4fa4eebc
Nikola <nsretkovic@elfak.rs>
2021-12-12 03:51:03 +0100

    Change printGameMatrix to accept only state as a parameter

--- UIController.py
+++ UIController.py
@@ -5,35 +5,35 @@ from External_Packages.ColorizeText import colored
 
 clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 
-def printGameMatrix(rows, cols, playerFields, hWalls, vWalls):
+def printGameMatrix(state):
   h_Wals =""
   fields_and_V_Wals = ""
   h_fieldNumbers = " "
-  for i in range(0, rows+1):
-    for j in range(0, cols+1):
+  for i in range(0, state.rows+1):
+    for j in range(0, state.cols+1):
       # Stampaj samo = = = za gornju ivicu
       if(i == 0):
-        if(j < cols):
+        if(j < state.cols):
           h_fieldNumbers+= str(j+1) if (j+1 < 10) else str(chr(65+j-9))
           h_fieldNumbers+= "  "
         #h_Wals += hWalls[i][j] +" "
-        h_Wals += hWalls[i][j] if hWalls[i][j] == "--" else colored(hWalls[i][j], 'green')
+        h_Wals += state.hWalls[i][j] if state.hWalls[i][j] == "--" else colored(state.hWalls[i][j], 'green')
         h_Wals += " " 
       if(i > 0):
         #fields_and_V_Wals += playerFields[i][j] + vWalls[i][j]
         playerColored = ""
-        if "X" in playerFields[i][j]:
-          playerColored = colored(playerFields[i][j], 'red', attrs=['bold'])
-        elif "O" in playerFields[i][j]:
-          playerColored = colored(playerFields[i][j], 'yellow', attrs=['bold'])
+        if "X" in state.playingFields[i][j]:
+          playerColored = colored(state.playingFields[i][j], 'red', attrs=['bold'])
+        elif "O" in state.playingFields[i][j]:
+          playerColored = colored(state.playingFields[i][j], 'yellow', attrs=['bold'])
         else:
-          playerColored = playerFields[i][j]
+          playerColored = state.playingFields[i][j]
         fields_and_V_Wals += playerColored
-        fields_and_V_Wals += vWalls[i][j] if vWalls[i][j] == "|" else colored(vWalls[i][j], 'blue')
+        fields_and_V_Wals += state.vWalls[i][j] if state.vWalls[i][j] == "|" else colored(state.vWalls[i][j], 'blue')
         #h_Wals +=hWalls[i][j] + " "
-        h_Wals += hWalls[i][j] if hWalls[i][j] == "--" else colored(hWalls[i][j], 'green')
+        h_Wals += state.hWalls[i][j] if state.hWalls[i][j] == "--" else colored(state.hWalls[i][j], 'green')
         h_Wals += " " 
-    if(j == cols):
+    if(j == state.cols):
       fields_and_V_Wals += " "
       fields_and_V_Wals += str(i) if (i < 10) else str(chr(65+i-10))
     if(i==0):
